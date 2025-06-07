921b19e260abfb27c665c5eb959eb739ef79b98f
Elsie <156831523+oakgreybookshelf@users.noreply.github.com>
2025-06-06 23:39:11 -0400

    Update Recursion.py

--- Recursion.py
+++ Recursion.py
@@ -8,3 +8,14 @@ def multiply(a, b):
 a = int(input("enter 1st number"))
 b = int(input("enter last number"))
 print("the product of", a, "and", b, "is", multiply(a, b))
+
+# exponent
+def raise_to_power(a, b):
+  c = 1
+    for i in range(b):
+    c *= b
+  raise_to_power(a-1, b)
+  return(c)
+a = int(input("enter 1st number"))
+b = int(input("enter last number"))
+print( a, "to the power of", b, "is", raise_to_power(a, b))
