706f2db2a1416e4fd9e6f3eb790bbcfd03e4cbb9
Elsie <156831523+oakgreybookshelf@users.noreply.github.com>
2025-06-06 23:42:18 -0400

    Update Recursion.py

--- Recursion.py
+++ Recursion.py
@@ -19,3 +19,76 @@ def raise_to_power(a, b):
 a = int(input("enter 1st number"))
 b = int(input("enter last number"))
 print( a, "to the power of", b, "is", raise_to_power(a, b))
+
+
+def triangle_of_stars(n):
+    if n > 0:
+        triangle_of_stars(n-1)
+        print("*" * n)
+        
+        
+def list_length(my_list):
+    count = 1
+    if my_list == []:
+        return 0
+    else:
+       
+        return count + list_length(my_list[1:])
+    
+    
+def number_of_digits(integer):
+    count = 1
+    if integer == 0:
+        return 0
+    else:
+        return count + number_of_digits(integer // 10)
+    
+def capitalize(string):
+
+    if string == "":
+        return ""
+    else:
+        return string[0].upper() + capitalize(string[1:])
+    
+
+def count_characters(string, char):
+    count = 0
+    if string == "":
+        return 0
+    if string[0] == char:
+        count = count + 1
+    return count + count_characters(string[1:], char)
+
+
+    
+def main():
+    n = int(input("Enter an intger: "))
+    triangle_of_stars(n)
+    
+    print()
+    
+    my_list = input("Enter elements seperated by a comma: ").split(",")
+    list_len = list_length(my_list)
+    print("Your list has", list_len, "elements")
+    
+    print()
+    
+    integer = int(input("Enter an integer: "))
+    num_digits = number_of_digits(integer)
+    print("The integer", integer, "has", num_digits, "number(s) of digits")
+    
+    print()
+    
+    string = input("Enter a string: ")
+    string_cap = capitalize(string)
+    print(string_cap)
+    
+    print()
+    
+    string = input("Enter a string: ")
+    char = input("Enter a single character: ")
+    count = count_characters(string, char)
+    print("The character ", "'", char, "'", " appears in the string ", "'", string, "' ", count, " time(s)", sep="")
+    
+main()
+    
